48d3ebd14f38e96f0ae035d44f5b52dfc6c2bb3d
Lionel Leiva-Marcon <llm@proactive.swiss>
2022-01-25 07:33:51 +0100

    25.01.2022 GA

--- src/boot/google-analytics.js
+++ src/boot/google-analytics.js
@@ -1,7 +1,8 @@
+import { uid } from 'quasar'
 import ga from '../services/analytics.js'
 
 export default ({ router }) => {
   router.afterEach((to, from) => {
-    ga.logPage(to.path, to.name, sessionId)
+    ga.logPage(to.path, to.name, uid())
   })
 }
